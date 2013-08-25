347fc51bc29c3d21588c8cf1145d10144d1f6d55
CaptainJH <heqiju@gmail.com>
2013-08-25 22:56:50 +0800

    refactoring the project, phrase 2 , finished

---
+++ EngineFoundation/jhViewController.mm
@@ -0,0 +1,140 @@
+//
+//  jhViewController.m
+//  GLKit_test
+//
+//  Created by JuHeQi on 13-8-17.
+//  Copyright (c) 2013年 JU Heqi. All rights reserved.
+//
+
+#import "jhViewController.h"
+#import "Surface.h"
+
+
+@interface jhViewController () {
+
+    float _rotation;
+    
+    Drawable* _drawable;
+}
+@property (strong, nonatomic) EAGLContext *context;
+@property (strong, nonatomic) GLKBaseEffect *effect;
+
+- (void)setupGL;
+- (void)tearDownGL;
+
+@end
+
+@implementation jhViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+
+    if (!self.context) {
+        NSLog(@"Failed to create ES context");
+    }
+    
+    GLKView *view = (GLKView *)self.view;
+    view.context = self.context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [self setupGL];
+}
+
+- (void)dealloc
+{    
+    [self tearDownGL];
+    
+    if ([EAGLContext currentContext] == self.context) {
+        [EAGLContext setCurrentContext:nil];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+
+    if ([self isViewLoaded] && ([[self view] window] == nil)) {
+        self.view = nil;
+        
+        [self tearDownGL];
+        
+        if ([EAGLContext currentContext] == self.context) {
+            [EAGLContext setCurrentContext:nil];
+        }
+        self.context = nil;
+    }
+
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setupGL
+{
+    [EAGLContext setCurrentContext:self.context];
+        
+    self.effect = [[GLKBaseEffect alloc] init];
+    self.effect.light0.enabled = GL_TRUE;
+    self.effect.light0.diffuseColor = GLKVector4Make(1.0f, 0.4f, 0.4f, 1.0f);
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    //_drawable = new BoxDrawable();
+    _drawable = new SphereDrawable();
+    
+    self.preferredFramesPerSecond = 60;
+}
+
+- (void)tearDownGL
+{
+    [EAGLContext setCurrentContext:self.context];
+    
+    if(_drawable)
+    {
+        delete _drawable;
+    }
+    
+    self.effect = nil;
+}
+
+#pragma mark - GLKView and GLKViewController delegate methods
+
+- (void)update
+{
+    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 0.1f, 100.0f);
+    
+    self.effect.transform.projectionMatrix = projectionMatrix;
+    
+    GLKMatrix4 baseModelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -4.0f);
+    baseModelViewMatrix = GLKMatrix4Rotate(baseModelViewMatrix, _rotation, 0.0f, 1.0f, 0.0f);
+    
+    // Compute the model view matrix for the object rendered with GLKit
+    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -1.5f);
+    //modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, _rotation, 1.0f, 1.0f, 1.0f);
+    modelViewMatrix = GLKMatrix4Identity;
+    modelViewMatrix = GLKMatrix4Multiply(baseModelViewMatrix, modelViewMatrix);
+    
+    self.effect.transform.modelviewMatrix = modelViewMatrix;
+    
+    _rotation += self.timeSinceLastUpdate * 0.5f;
+    
+    self.textDisplay.text = [NSString stringWithFormat:@"%d", self.framesPerSecond];
+    
+    //////
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+    glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // Render the object with GLKit
+    [self.effect prepareToDraw];
+    
+    _drawable->draw();
+    
+}
+
+@end
